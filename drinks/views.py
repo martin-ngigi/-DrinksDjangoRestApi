850294b8857a8c9db26aafff6ca16f18ac005c19
martin-ngigi <martinwainaina001@gmail.com>
2022-10-06 17:35:18 +0300

    PUT i.e update data

--- drinks/views.py
+++ drinks/views.py
@@ -3,11 +3,15 @@
 
 from dataclasses import dataclass
 from django.http import JsonResponse
+from django.urls import is_valid_path
 from .models import Drink
 from .serializers import DrinkSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response  import Response
 from rest_framework import status
+
+# Post -> insert new record(Create)
+# Put -> update data
 
 #can accept either get or post
 #@api_view('GET', 'POST')
@@ -45,3 +49,24 @@ def drink_list(request):
 
         #2. decerialize it (convert from json to python)
         #3. return python data
+
+@api_view(['GET', 'PUT', 'DELET'])
+def drink_detail(request, id): #id is frpm urls.py
+    try:
+        drink = Drink.objects.get(pk=id)
+    except Drink.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = DrinkSerializer(drink)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = DrinkSerializer(drink, data=request.data)
+        # if serializer is_valid save data
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        #else show error message
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        pass
